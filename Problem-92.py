b8aff0967207fb6a02886ac0ba6c2811880e0a23
Xiaotao Wang <wangxiaotao868@163.com>
2016-03-29 18:48:33 +0800

    Problem-92 is solved
    I cannot devise more efficient algorithm for this problem. Current
    algorithm takes about 41.2s.

--- Problem-92.py
+++ Problem-92.py
@@ -23,3 +23,47 @@ or 89.
 How many starting numbers below ten million will arrive at 89?
 
 """
+def splitnum(n):
+    
+    digits = [int(i) for i in str(n) if i!='0']
+    digits.sort()
+    
+    return tuple(digits)
+
+def sumsuqare(digits):
+    
+    return sum(d**2 for d in digits)
+
+def count(maxnum = 10000000):
+    
+    cache = {}
+    stat = 0
+    for n in xrange(1, maxnum+1):
+        Next = splitnum(n)
+        tmp = [Next]
+        while True:
+            if Next in cache:
+                for d in tmp:
+                    cache[d] = cache[Next]
+                    
+                if cache[Next] == 89:
+                    stat += 1
+                break
+        
+            check = sumsuqare(Next)
+            if (check == 89) or (check == 1):
+                for d in tmp:
+                    cache[d] = check
+                if check == 89:
+                    stat += 1
+                break
+            
+            Next = splitnum(check)
+            tmp.append(Next)
+    
+    return stat, cache
+
+if __name__ == '__main__':
+    res = count(10000000)
+                
+            
