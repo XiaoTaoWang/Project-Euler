85d69f9b140b52c5d3f3c3a26f29552958e086fa
Xiaotao Wang <wangxiaotao868@163.com>
2016-04-17 18:04:04 +0800

    Problem 62 is solved

--- Problem-62.py
+++ Problem-62.py
@@ -16,3 +16,28 @@ Find the smallest cube for which exactly five permutations of its digits are
 cube.
 
 """
+from collections import defaultdict
+
+def numtokey(num):
+    numlist = list(map(str, str(num)))
+    numlist.sort()
+    return int(''.join(numlist[::-1]))
+
+def findcube(start):
+    
+    counter = defaultdict(int)
+    D = {}
+    while True:
+        cube = start ** 3
+        key = numtokey(cube)
+        if not key in D:
+            D[key] = start
+        counter[key] += 1
+        if counter[key] == 5:
+            return key, D[key]
+        
+        start += 1
+
+if __name__ == '__main__':
+    res = findcube(345)
+        
